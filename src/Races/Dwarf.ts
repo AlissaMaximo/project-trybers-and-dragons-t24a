d4e630abce61ba1ed8cb759c0c872cb8ebf60f20
Alissa Maximo <alissahiratamaximo@gmail.com>
2023-03-14 23:33:01 -0300

    Constructor do Dwarf feito

--- src/Races/Dwarf.ts
+++ src/Races/Dwarf.ts
@@ -2,8 +2,18 @@ import Race from './Race';
 
 export default class Dwarf extends Race {
   private _maxLifePoints = 80;
+  private static _instance = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    Dwarf._instance += 1;
+  }
 
   get maxLifePoints(): number {
     return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    return this._instance; // retorna _instance para a classe Race
   }
 }
