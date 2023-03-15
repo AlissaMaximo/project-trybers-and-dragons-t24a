3d078607de7632505a51c52f52ff36804e874882
Alissa Maximo <alissahiratamaximo@gmail.com>
2023-03-15 16:39:36 -0300

    Atributos da classe Character criados

---
+++ src/Character.ts
@@ -0,0 +1,14 @@
+import Race from './Races';
+import Archetype from './Archetypes';
+import Energy from './Energy';
+
+export default class Character {
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
+}
